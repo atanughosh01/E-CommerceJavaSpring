c1d187270c09f5e3ca036910de865232d8caf25a
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2022-04-24 03:04:47 +0530

    added service

---
+++ src/main/java/SpringDB/service/Buy.java
@@ -0,0 +1,24 @@
+package SpringDB.service;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import SpringDB.schema.Apparel;
+import SpringDB.schema.Users;
+
+@Service
+public class Buy {
+
+	// If user buys an apparel, associate it with the user and set the order type
+	// according to the items bought
+	public Users buy(Apparel a, Users u) {
+		Order o = new Order();
+		Set<Apparel> ap = u.getAp();
+		ap.add(a);
+		u.setAp(ap);
+		if (a.getType().equals("Seasonal"))
+			o.seasonal(u);
+		else
+			o.newArrival(u);
+		return u;
+	}
+}
---
+++ src/main/java/SpringDB/service/Order.java
@@ -0,0 +1,40 @@
+package SpringDB.service;
+
+import org.springframework.stereotype.Service;
+import SpringDB.schema.History;
+import SpringDB.schema.Users;
+
+@Service
+public class Order {
+
+	// If the user has no history (neither seasonal nor new-arrival), create a new
+	// history and set the user accordingly
+	public void set(Users u) {
+		History h = null;
+		if (u.getH() == null) {
+			h = new History();
+			u.setH(h);
+			h.setU(u);
+		}
+	}
+
+	// For seasonal items, create the item, increment the count, and set the user
+	// type as seasonal
+	public Users seasonal(Users u) {
+		set(u);
+		History h = u.getH();
+		h.incS();
+		u.setH(h);
+		return u;
+	}
+
+	// For new-arrival items, create the item, increment the count, and set the user
+	// type as new-arrival
+	public Users newArrival(Users u) {
+		set(u);
+		History h = u.getH();
+		h.incN();
+		u.setH(h);
+		return u;
+	}
+}
